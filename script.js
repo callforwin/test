ea90547d8ef964c8da5e98dd96d3684dde977cb8
callforwin <139757434+callforwin@users.noreply.github.com>
2023-08-22 00:19:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+document.getElementById('subscribe-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var email = document.getElementById('email').value;
+    alert('Hvala na prijavi, ' + email + '! Uskoro ćete primiti obavijesti od nas.');
+    // Ovdje možete dodati logiku za spremanje e-maila u bazu podataka ili slanje na server
+});
