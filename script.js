caea6dfa990d647d7a3292641d0be02ce489d742
callforwin <139757434+callforwin@users.noreply.github.com>
2023-08-22 00:29:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ document.getElementById('subscribe-form').addEventListener('submit', function(e)
     e.preventDefault();
     var email = document.getElementById('email').value;
     alert('Hvala na prijavi, ' + email + '! Uskoro ćete primiti obavijesti od nas.');
+    var images = [
+    'https://source.unsplash.com/random/1920x1080/?apps',
+    'https://source.unsplash.com/random/1920x1080/?influencers',
+    'https://source.unsplash.com/random/1920x1080/?crowdfunding'
+];
+var currentImage = 0;
+
+function changeImage() {
+    document.querySelector('.hero').style.backgroundImage = 'url(' + images[currentImage] + ')';
+    currentImage = (currentImage + 1) % images.length;
+}
+
+setInterval(changeImage, 5000); // mijenja sliku svakih 5 sekundi
     // Ovdje možete dodati logiku za spremanje e-maila u bazu podataka ili slanje na server
 });
