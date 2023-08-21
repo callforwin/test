f237e86e7bc9ad271cd8a5531d97f4a802541908
callforwin <139757434+callforwin@users.noreply.github.com>
2023-08-22 00:49:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-// Funkcionalnost za rotirajuće slike
 var images = [
     'https://source.unsplash.com/random/1920x1080/?apps',
     'https://source.unsplash.com/random/1920x1080/?influencers',
     'https://source.unsplash.com/random/1920x1080/?crowdfunding'
 ];
-var currentImage = 0;
-
-function changeImage() {
-    var heroElement = document.querySelector('.hero');
-    if (heroElement) {
-        heroElement.style.backgroundImage = 'url(' + images[currentImage] + ')';
-        currentImage = (currentImage + 1) % images.length;
-    } else {
-        console.error('Element s klasom .hero nije pronađen');
-    }
-}
+var randomImage = images[Math.floor(Math.random() * images.length)];
 
 window.onload = function() {
-    setInterval(changeImage, 5000); // mijenja sliku svakih 5 sekundi
+    document.querySelector('.hero').style.backgroundImage = 'url(' + randomImage + ')';
 };
