3eaf77b0fa2d74a5fee4252191485f34ca201804
callforwin <139757434+callforwin@users.noreply.github.com>
2023-08-22 00:36:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-// Funkcionalnost za formu za prijavu
-document.getElementById('subscribe-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var email = document.getElementById('email').value;
-    alert('Hvala na prijavi, ' + email + '! Uskoro ćete primiti obavijesti od nas.');
-    // Ovdje možete dodati logiku za spremanje e-maila u bazu podataka ili slanje na server
-});
+// Funkcija koja će se pozvati nakon što se stranica učita
+function onPageLoad() {
+    // Funkcionalnost za formu za prijavu
+    document.getElementById('subscribe-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        var email = document.getElementById('email').value;
+        alert('Hvala na prijavi, ' + email + '! Uskoro ćete primiti obavijesti od nas.');
+        // Ovdje možete dodati logiku za spremanje e-maila u bazu podataka ili slanje na server
+    });
 
-// Funkcionalnost za rotirajuće slike
-var images = [
-    'https://source.unsplash.com/random/1920x1080/?apps',
-    'https://source.unsplash.com/random/1920x1080/?influencers',
-    'https://source.unsplash.com/random/1920x1080/?crowdfunding'
-];
-var currentImage = 0;
+    // Funkcionalnost za rotirajuće slike
+    var images = [
+        'https://source.unsplash.com/random/1920x1080/?apps',
+        'https://source.unsplash.com/random/1920x1080/?influencers',
+        'https://source.unsplash.com/random/1920x1080/?crowdfunding'
+    ];
+    var currentImage = 0;
 
-function changeImage() {
-    document.querySelector('.hero').style.backgroundImage = 'url(' + images[currentImage] + ')';
-    currentImage = (currentImage + 1) % images.length;
+    function changeImage() {
+        document.querySelector('.hero').style.backgroundImage = 'url(' + images[currentImage] + ')';
+        currentImage = (currentImage + 1) % images.length;
+    }
+
+    setInterval(changeImage, 5000); // mijenja sliku svakih 5 sekundi
 }
 
-setInterval(changeImage, 5000); // mijenja sliku svakih 5 sekundi
+// Dodajemo slušatelj koji će pozvati funkciju onPageLoad nakon što se stranica učita
+window.addEventListener('load', onPageLoad);
