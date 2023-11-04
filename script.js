8c06104126fc59fc1f21359c86f27c6607eba9e4
callforwin <139757434+callforwin@users.noreply.github.com>
2023-11-04 23:50:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-var images = [
-    'https://source.unsplash.com/random/1920x1080/?apps',
-    'https://source.unsplash.com/random/1920x1080/?influencers',
-    'https://source.unsplash.com/random/1920x1080/?crowdfunding'
-];
-var randomImage = images[Math.floor(Math.random() * images.length)];
-
-window.onload = function() {
-    document.querySelector('.hero').style.backgroundImage = 'url(' + randomImage + ')';
-};
+function podrziProjekt() {
+    // Ovdje možete dodati logiku za otvaranje forme za donaciju ili preusmjeravanje na stranicu za plaćanje
+    alert('Hvala na podršci! Molimo vas da pratite upute za donaciju.');
+}
