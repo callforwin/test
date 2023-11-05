27f67262c5049749924a8dda5d5e9e8c24a4625c
callforwin <139757434+callforwin@users.noreply.github.com>
2023-11-05 02:51:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-function podrziProjekt() {
-    // Ovdje možete dodati logiku za otvaranje forme za donaciju ili preusmjeravanje na stranicu za plaćanje
-    alert('Hvala na podršci! Molimo vas da pratite upute za donaciju.');
-}
+
